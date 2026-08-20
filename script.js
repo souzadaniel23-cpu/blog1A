bbb44fdb4d7d0a7fa55f589d93835c6b60b14b72
souzadaniel23-cpu <souza.daniel23@escola.pr.gov.br>
2026-08-20 15:14:20 +0000

    4657hgrhnr

---
+++ script.js
@@ -0,0 +1,12 @@
+const botaolike = document.querySelector (".like");
+
+botaolike.addEventListener ("click,like");
+function() {
+console.log ("foi clickdo");
+
+let texto= botaolike.querySelector  ("span");
+texto.textContent++;
+
+
+
+}
